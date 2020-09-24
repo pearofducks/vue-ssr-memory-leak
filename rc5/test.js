db4c5388f4f37501a0e31e61f37b2c05ea291b46
Dave Honneffer <pearofducks@gmail.com>
2020-09-24 14:58:17 +0200

    Initial

---
+++ rc5/test.js
@@ -0,0 +1,48 @@
+// const http = require('http')
+const { createSSRApp, h } = require('vue')
+const { renderToString } = require('@vue/server-renderer')
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const component = {
+  template: '<h1>Hello {{ name }}</h1>',
+  props: {
+    name: String
+  }
+}
+
+const createApp = (...args) => createSSRApp({ render: () => h(...args) })
+
+const run = async () => {
+  const html = await renderToString(createApp(component, { name: 'world' }))
+  return html
+}
+
+const loop = async () => {
+  const startMem = Math.round((process.memoryUsage().rss / 1048576) * 100) / 100;
+  console.log("STARTING", startMem)
+
+  for (let i = 0; i < 100000; i++) {
+    await run()
+  }
+
+  const endMem = Math.round((process.memoryUsage().rss / 1048576) * 100) / 100;
+  console.log("ENDING", endMem)
+
+  console.log("SLEEPING")
+  await sleep(10000)
+
+  const endMemCleaned = Math.round((process.memoryUsage().rss / 1048576) * 100) / 100;
+  console.log("ENDING", endMemCleaned)
+}
+
+loop()
+
+// const server = http.createServer(async (req, res) => {
+//   const content = await run()
+//   res.end(content)
+// })
+
+// server.listen(3000)
